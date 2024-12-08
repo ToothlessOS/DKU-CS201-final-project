dde9bad401bcb3f9a54361f2e9fdbff95a43251c
ToothlessOS <anthony.13661499167@gmail.com>
2024-12-08 16:07:42 +0800

    Fix 'getMostFrequentWords'

--- src/main/java/xyz/toothlessos/util/MyHashmap.java
+++ src/main/java/xyz/toothlessos/util/MyHashmap.java
@@ -3,13 +3,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MyHashmap {
-    private static class Entry {
+    public static class Entry {
         String key;
         int value;
 
         Entry(String key, int value) {
             this.key = key;
             this.value = value;
+        }
+
+        public String getKey() {
+            return key;
+        }
+
+        public int getValue() {
+            return value;
         }
     }
 
